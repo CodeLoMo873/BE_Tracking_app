a481f893b35ac68ba8edde7e0a3d145dcdbf9f46
hiep.bui <hiep.bui@ekotek.vn>
2025-04-16 00:51:22 +0700

    update api

--- controllers/trackController.js
+++ controllers/trackController.js
@@ -26,3 +26,23 @@ exports.getSuggestedTracks = async (req, res) => {
   const tracks = await Track.find({ weatherTags: weather.toLowerCase() }).populate('artist album');
   res.json(tracks);
 };
+
+exports.getTrackById = async (req, res) => {
+  try {
+    const track = await Track.findById(req.params.id).populate(['artists', 'album']);
+    if (!track) return res.status(404).json({ error: 'Track not found' });
+    res.status(200).json(track);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getTracksByAlbum = async (req, res) => {
+  const { albumId } = req.params;
+  try {
+    const tracks = await Track.find({ album: albumId }).populate(['artists', 'album']);
+    res.status(200).json(tracks);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
--- routes/trackRoutes.js
+++ routes/trackRoutes.js
@@ -5,5 +5,6 @@ const trackController = require('../controllers/trackController');
 router.post('/', trackController.createTrack);
 router.get('/', trackController.getTracks);
 router.get('/suggest', trackController.getSuggestedTracks); // ?weather=sunny
-
+router.get('/:id', trackController.getTrackById);
+router.get('/album/:albumId', trackController.getTracksByAlbum);
 module.exports = router;
