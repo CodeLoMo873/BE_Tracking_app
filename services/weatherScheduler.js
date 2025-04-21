4464d3fe65a5634b76857fd5740a5c1f2c442e1f
CwtchMH <nmhhieuk2@gmail.com>
2025-04-21 10:37:34 +0700

    trying to save data in database

--- services/weatherScheduler.js
+++ services/weatherScheduler.js
@@ -1,22 +1,24 @@
-const cron = require('node-cron');
-const weatherController = require('../controllers/weatherController');
+const cron = require('node-cron')
+const weatherController = require('../controllers/weatherController')
 
 // Initialize the weather scheduler
 const initWeatherScheduler = () => {
   // Schedule to run every 15 minutes
   cron.schedule('*/15 * * * *', async () => {
-    console.log('Running scheduled weather update - ' + new Date().toLocaleString());
+    console.log(
+      'Running scheduled weather update - ' + new Date().toLocaleString()
+    )
     try {
-      await weatherController.updateWeatherData({ query: { city: 'Hanoi' } });
-      console.log('Weather data successfully updated');
+      await weatherController.updateWeatherData({ query: { city: 'Hanoi,vn' } })
+      console.log('Weather data successfully updated')
     } catch (error) {
-      console.error('Failed to update weather data:', error);
+      console.error('Failed to update weather data:', error)
     }
-  });
-  
-  console.log('Weather update scheduler initialized');
-};
+  })
+
+  console.log('Weather update scheduler initialized')
+}
 
 module.exports = {
   initWeatherScheduler
-};
+}
