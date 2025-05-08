35d16c503b4f1ad10d541d47b46f932eea823cd5
CwtchMH <nmhhieuk2@gmail.com>
2025-05-08 22:58:31 +0700

    Done

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 
 app.use(express.urlencoded({ extended: true }))
 
